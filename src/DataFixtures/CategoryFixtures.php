e10e87e05f3c2e162a7d8e93202f7fc925ebaac0
Killian <killian.couraillon@hotmail.fr>
2021-07-21 14:54:26 +0200

    add fixtures for category and aliment

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,12 @@ class CategoryFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 1; $i<=10; $i++) {
+            $category = new Category();
+            $category->setName('category_' . $i);
+            $manager->persist($category);
+            $this->addReference('category_' . $i, $category);
+        }
 
         $manager->flush();
     }
