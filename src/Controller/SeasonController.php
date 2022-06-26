ac06bf149ff623b9e4795c81610942acb9cb4fb1
Killian <killian.couraillon@hotmail.fr>
2022-06-26 22:18:31 +0200

    CRUD Season

---
+++ src/Controller/SeasonController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Season;
+use App\Form\SeasonType;
+use Doctrine\ORM\ORMException;
+use Doctrine\ORM\EntityManager;
+use App\Repository\SeasonRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\OptimisticLockException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @Route("/season")
+ */
+class SeasonController extends AbstractController
+{
+    /**
+     * @Route("/", name="season_index")
+     * @param SeasonRepository $seasonRepository
+     * @return Response
+     */
+    public function index(SeasonRepository $seasonRepository): Response
+    {
+        return $this->render('season/index.html.twig', [
+            'seasons' => $seasonRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="season_new")
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @return Response
+     */
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $season = new Season();
+        $seasonType = $this->createForm(SeasonType::class, $season);
+        $seasonType->handleRequest($request);
+        if ($seasonType->isSubmitted() && $seasonType->isValid()) {
+            $em ->persist($season);
+            $em ->flush($season);
+            return $this->redirectToRoute('season_index');
+        }
+
+
+        return $this->render('season/new.html.twig', [
+            'form' => $seasonType->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="season_edit")
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @param Season $season
+     * @return Response
+     */
+    public function edit(Request $request, EntityManagerInterface $em, Season $season): Response
+    {
+        $seasonType = $this->createForm(SeasonType::class, $season);
+        $seasonType->handleRequest($request);
+        if ($seasonType->isSubmitted() && $seasonType->isValid()) {
+            $em ->flush($season);
+            return $this->redirectToRoute('season_index');
+        }
+
+
+        return $this->render('season/new.html.twig', [
+            'season' => $season,
+            'form'   => $seasonType->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="season_delete")
+     * @param EntityManager $em
+     * @param Season $season
+     * @return Response
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function delete(EntityManagerInterface $em, Season $season): Response
+    {
+        $em->remove($season);
+        $em->flush();
+        return $this->redirectToRoute('season_index');
+    }
+}
